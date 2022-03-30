ca8e2fa9b081b2703ead176fff8c4aceeb011335
HoYaBam <101304086+HoYaBam@users.noreply.github.com>
2022-03-30 15:56:37 +0900

    조금 더 깊숙한 스프링부트

---
+++ workspace/Chapter01/src/main/java/com/ruby/OtherController.java
@@ -0,0 +1,10 @@
+package com.ruby;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OtherController {
+	public OtherController() {
+		System.out.println("(☞ﾟヮﾟ)☞ OtherController 생성");
+	}
+}
